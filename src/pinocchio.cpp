e71417befbf476c000bd8dd0621ac63cf228288b
shyblue <rickyjang@gmail.com>
2015-12-23 22:05:56 +0900

    refs #1366 API End point implementation
    * Add end point route logic.

--- src/pinocchio.cpp
+++ src/pinocchio.cpp
@@ -1,6 +1,9 @@
 #include "pinocchio.h"
 #include "config.h"
 
+#include <boost/algorithm/string.hpp>
+
+
 using StringOpt =  boost::optional<std::string>;
 
 TPinocchio::TPinocchio(const std::string& ip, const std::string& port) : m_app(),m_ip(ip),m_port(port)
@@ -15,11 +18,27 @@ bool TPinocchio::Initialize()
 
 bool TPinocchio::AddRouter()
 {
-	CROW_ROUTE(m_app,"/").name("hello")
-	( [] { return "Hello World!"; } );
+	CROW_ROUTE(m_app,"/").name("pinocchio")
+	( [] { return "This is pinocchio!"; } );
 
-	CROW_ROUTE(m_app,"/send_msg").methods("POST"_method)
+	CROW_ROUTE(m_app,"/gcm/send").methods("POST"_method)
 	([](const crow::request& req)
+		{
+			std::string server_key = req.get_header_value("Authorization");
+
+			std::ostringstream os;
+
+			std::vector<std::string> fields;
+			boost::split(fields, server_key, boost::is_any_of("="));
+			for (const auto& field : fields)
+				os << "[" << field << "]";
+
+			return crow::response{os.str()};
+		}
+	);
+
+	CROW_ROUTE(m_app,"/regist/<string>")
+	([](const crow::request& req,const std::string& token)
 	 {
 	 	auto x = crow::json::load(req.body);
 
@@ -31,11 +50,19 @@ bool TPinocchio::AddRouter()
 	 }
 	 );
 
+	CROW_ROUTE(m_app,"/gcm/recv/<string>")
+	([](const std::string& token)
+ 	 {
+		 if(token.length()!=10) return crow::response(400);
+		 return crow::response(200);
+	 }
+	);
+
 	return true;
 }
 bool TPinocchio::run()
 {
-	m_app.port(std::stoi(m_port))
+	m_app.port(static_cast<uint16_t >(std::stoi(m_port)))
 		.multithreaded()
 		.run();
 
