098a693dc8195de267d409d8632849223490508f
Ricky Jang <rickyjang@gmail.com>
2016-05-03 12:21:24 +0900

    Update common.h
    Add finalize macro

--- src/common.h
+++ src/common.h
@@ -19,3 +19,21 @@
 #define KBYTEUNIT 10
 #define MBYTEUNIT 20
 #define GBYTEUNIT 30
+
+template< typename T >
+class at_block_exit_finalizer_holder
+{
+	at_block_exit_finalizer_holder( const T& fn ) : m_finalizer( &fn ) {}
+	at_block_exit_finalizer_holder( const at_block_exit_finalizer_holder& a ) : m_finalizer( a.m_finalizer ) { a.m_finalizer = nullptr; }
+	~at_block_exit_finalizer_holder() { if ( m_finalizer ) (*m_finalizer)(); }
+private:
+	void operator = ( const at_block_exit_finalizer_holder& );
+	mutable const T* m_finalizer;
+};
+
+template< typename T > static inline at_block_exit_finalizer_holder<T> at_block_exit( const T& fn )
+{
+	return at_block_exit_finalizer_holder<T>(fn);
+}
+
+#define AFC_FINALLY( ... ) auto _at_block_exit_##__FUNCTION__##__LINE__ = at_block_exit( [&]() __VA_ARGS__ )
